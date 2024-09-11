ada4568cccc520462158f6a2c7fa104a3bdcce3f
thopn3 <thopn3@fpt.edu.vn>
2024-09-11 11:28:22 +0700

    Layout - Login form Demo

---
+++ MyFirstApp/MyFirstApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyFirstApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnExit(ExitEventArgs e)
+        {
+            //MessageBox.Show("Do you want exit?");
+        }
+    }
+
+}
---
+++ WindowPage_WPF/WindowPage_WPF/FrmLogin.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowPage_WPF
+{
+    /// <summary>
+    /// Interaction logic for FrmLogin.xaml
+    /// </summary>
+    public partial class FrmLogin : Window
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkLogin())
+            {
+                MessageBox.Show("Login success");
+                // Check user, pass -> DB
+            }
+        }
+
+        private bool checkLogin()
+        {
+            bool status = true;
+            if(txtUsername.Text.Trim().Length == 0)
+            {
+                status = false;
+                lbMsgUsername.Content = "Username is required";
+            }
+            else
+            {
+                lbMsgUsername.Content = "";
+            }
+
+            if (txtPassword.Password.Trim().Length == 0)
+            {
+                status = false;
+                lbMsgPassword.Content = "Password is required";
+            }
+            else
+            {
+                lbMsgPassword.Content = "";
+            }
+
+            if (!status)
+                return false;
+            else
+                return true;
+        }
+    }
+}
